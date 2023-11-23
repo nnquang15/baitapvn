23683c92ee5707a51af72b29649a76388a9995da
nnquang15 <151819280+nnquang15@users.noreply.github.com>
2023-11-23 14:45:43 +0700

    insert data

---
+++ SQLQuery.sql
@@ -0,0 +1,12 @@
+create database SinhVien
+use SinhVien
+
+create table ttSV(
+MaSV nvarchar(5) primary key,
+TenSV nvarchar(50),
+GioiTinh nvarchar(5),
+Ngaysinh date,
+Lop nvarchar(10),
+DTB float,
+
+)
